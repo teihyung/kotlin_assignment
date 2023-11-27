f5f0c462460716de86c8ca73049617060e60b90b
Tae Hyung Lee <teihyung@naver.com>
2023-11-27 12:19:37 -0800

    finished implementing up to fetching

--- app/src/main/java/com/bcit/myapp/data/ApiEndPoints.kt
+++ app/src/main/java/com/bcit/myapp/data/ApiEndPoints.kt
@@ -1,10 +1,9 @@
 package com.bcit.myapp.data
 
-enum class ApiEndPoints
-    (
-    val url: String
-) {
-    BASE_URL("https://bible-api.com/"),
-    ARTWORK("${BASE_URL.url}/artworks"),
-    FIELDS("${ARTWORK.url}?fields=id,title,image_id")
+enum class ApiEndPoints(val url: String) {
+    BASE_URL("https://bible-api.com/");
+
+    fun verseUrl(reference: String): String {
+        return "${BASE_URL.url}$reference"
+    }
 }
--- app/src/main/java/com/bcit/myapp/data/BibleRepository.kt
+++ app/src/main/java/com/bcit/myapp/data/BibleRepository.kt
@@ -6,20 +6,24 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-class BibleRepository(
-    private val client: HttpClient
-){
+class BibleRepository(private val client: HttpClient) {
 
-    suspend fun getBible() : Bible{
-        val response = client.get(ApiEndPoints.FIELDS.url)
-
-        val json = response.body<JsonObject>().toString()
+    suspend fun getBibleVerse(reference: String): Bible {
+        val url = ApiEndPoints.BASE_URL.verseUrl(reference)
+        val response = client.get(url)
+        val json = response.body<String>()
 
         return deserializeJson(json)
     }
 
     private fun deserializeJson(json: String): Bible {
-        return Gson().fromJson(json, Bible::class.java)
-    }
+        val jsonObject = Gson().fromJson(json, JsonObject::class.java)
+        val versesArray = jsonObject.getAsJsonArray("verses") ?: throw IllegalArgumentException("No verses found")
 
+        val verses = versesArray.map { verseElement ->
+            Gson().fromJson(verseElement, BibleContext::class.java)
+        }
+
+        return Bible(verses)
+    }
 }
